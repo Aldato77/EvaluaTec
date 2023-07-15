d47fe78a2914afc2541f84b4191df47d6dc6f632
Aldato77 <aldairtorres28@outlook.com>
2023-07-15 15:32:10 -0600

    Se agrega el proyecto

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+     <div class="barra">
+            <img src="src/Img/logoheader.svg" alt="logo-gob">
+        </div>
+        <div class="icono-header container">
+            <div class="contenido-icono-logo">
+                <img src="src/Img/logo-gob.png" alt="">
+                <img src="src/Img/logo-educacion.png" alt="">
+                <img src="src/Img/logo-tecnm.png" alt="">
+                <img src="src/Img/logo-ita.png" alt="">
+            </div>
+            <div class="contenido-icono">
+                <img src="src/Img/icono-correo.png" alt="">
+                <img src="src/Img/buzon-icono.png" alt="">
+                <img src="src/Img/icono-calendario.png" alt="">
+                <img src="src/Img/icono-china.png" alt="">
+                <img src="src/Img/icono-usa.png" alt="">
+                <img src="src/Img/icono-francia.png" alt="">
+                <img src="src/Img/icono-mexico.png" alt="">
+            </div>
+        </div>
+        <h1>Maestría en Desarrollo Regional e Innovación Tecnológica</h1>
+</template>
---
+++ src/views/Login.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Header from '../components/Header.vue'
+import { FormKit } from '@formkit/vue'
+</script>
+
+<template>
+    <Header />
+    <div class="centrar-form container">
+        
+        <div class="titulo-Login">
+            <h2>Tecnologico Nacional de Mexico Campus Acapulco</h2>
+            <img src="../Img/logo-ita.png" alt="">
+        </div>
+        
+        <div class="formulario">
+            <div>
+                <h2 class="titulo-sesion">Iniciar Sesión</h2>
+                <img src="../Img/logo-ita.png" alt="">
+            </div>
+            <FormKit type="form">
+                <FormKit type="text" label="Correo Electronico" placeholder="Ingresa tu correro electronico"
+                    suffixIcon="email" />
+                <FormKit type="password" label="Contraseña" placeholder="Ingresa tu contraseña" suffixIcon="password" />
+            </FormKit>
+        </div>
+    </div>
+</template>
